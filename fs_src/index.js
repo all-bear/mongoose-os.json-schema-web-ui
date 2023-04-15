7ccae2500e87952f194bb3c19d4130b90f7fd955
Oleh Biriukov <ol.biriukov@gmail.com>
2023-04-15 21:15:00 +0300

    fix broken form

--- fs_src/index.js
+++ fs_src/index.js
@@ -3,83 +3,67 @@ import { initialize as initializeJSONEditor } from "./json-editor";
 import { loadConfig, rpcCall, saveConfig } from "./mongoose-os";
 import { setDeepValue, getDeepValue, fetchJSON } from "./utils"
 
-const WebUIConfigModeBehavior = {
-    "config": {
-        getSchema(config) {
-            return JSON.parse(config.jsonschemawebui.config.schema);
-        },
-        getDataFromConfig(config) {
-            return config;
-        },
-        getConfigFromData(data) {
-            return data;
-        }
+let WebUIConfigModeBehavior = {};
+
+WebUIConfigModeBehavior.config = {
+    getSchema(config) {
+        return JSON.parse(config.jsonschemawebui.config.schema);
     },
-    "customconfig": {
-        getSchema(config) {
-            return JSON.parse(config.jsonschemawebui.customconfig.schema);
-        },
-        getDataFromConfig(config) {
-            const mapping = JSON.parse(config.jsonschemawebui.customconfig.mapping);
-            const data = {};
-
-            for (const dataPath in mapping) {
-                const configPath = mapping[dataPath];
-
-                setDeepValue(data, getDeepValue(config, configPath), dataPath)
-            }
-
-            return data;
-        },
-        getConfigFromData(data, config) {
-            const mapping = JSON.parse(config.jsonschemawebui.customconfig.mapping);
-            const newConfig = {};
-
-            for (const dataPath in mapping) {
-                const configPath = mapping[dataPath];
-
-                setDeepValue(newConfig, getDeepValue(data, dataPath), configPath)
-            }
-
-            return newConfig;
-        }
+    getDataFromConfig(config) {
+        return config;
     },
-    "customconfigByUrl": {
-        async getFormConfigByUrl(config) {
-            const url = config.jsonschemawebui.customconfig.url;
-
-            return await fetchJSON(url, "GET");
-        },
-        async getSchema(config) {
-            return (await this.getFormConfigByUrl(config)).schema;
-        },
-        async getMapping(config) {
-            return (await this.getFormConfigByUrl(config)).mapping;
-        },
-        async getDataFromConfig(config) {
-            const formConfig = await this.getFormConfigByUrl(config);
-
-            return WebUIConfigModeBehavior["customconfig"].getDataFromConfig({
-                ...config,
-                jsonschemawebui: {
-                    ...config.jsonschemawebui,
-                    customconfig: formConfig,
-                }
-            })
-        },
-        async getConfigFromData(data, config) {
-            const formConfig = await this.getFormConfigByUrl(config);
-
-            return WebUIConfigModeBehavior["customconfig"].getConfigFromData(data,{
-                ...config,
-                jsonschemawebui: {
-                    ...config.jsonschemawebui,
-                    customconfig: formConfig,
-                }
-            })
-        }
+    getConfigFromData(data) {
+        return data;
     }
-}
+};
+
+WebUIConfigModeBehavior.customconfig = {
+    getSchema(config) {
+        return JSON.parse(config.jsonschemawebui.customconfig.schema);
+    },
+    getMapping(config) {
+        return JSON.parse(config.jsonschemawebui.customconfig.mapping);
+    },
+    async getDataFromConfig(config) {
+        const mapping = await this.getMapping(config);
+        const data = {};
+
+        for (const dataPath in mapping) {
+            const configPath = mapping[dataPath];
+
+            setDeepValue(data, getDeepValue(config, configPath), dataPath)
+        }
+
+        return data;
+    },
+    async getConfigFromData(data, config) {
+        const mapping = await this.getMapping(config);
+        const newConfig = {};
+
+        for (const dataPath in mapping) {
+            const configPath = mapping[dataPath];
+
+            setDeepValue(newConfig, getDeepValue(data, dataPath), configPath)
+        }
+
+        return newConfig;
+    }
+};
+
+WebUIConfigModeBehavior.customconfigByUrl = {
+    ...WebUIConfigModeBehavior.customconfig,
+    async getFormConfigByUrl(config) {
+        const url = config.jsonschemawebui.customconfig.url;
+
+        return await fetchJSON(url, "GET");
+    },
+    async getSchema(config) {
+        return (await this.getFormConfigByUrl(config)).schema;
+    },
+    async getMapping(config) {
+        return (await this.getFormConfigByUrl(config)).mapping;
+    }
+};
 
 loadConfig().then(async (config) => {
     const modeBehaviorName = config.jsonschemawebui.customconfig.url ? "customconfigByUrl" : (
